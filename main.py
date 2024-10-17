df017558c692f940c667bb21f33c4b1129522ecd
RenathaLoiza <143304174+RenathaLoiza@users.noreply.github.com>
2024-10-17 16:25:22 -0300

    Update main.py

--- main.py
+++ main.py
@@ -106,32 +106,35 @@ else:
    print("Você é maior de idade")
 
 print("##############################################################")
-#Calculadora simples
-print("Calculadora simples") 
 
-opcao=int(input("voce deseja 1-soma 2-subtração 3-multiplicação 4-divisão"))
+#Calculatora
+print("Calculadora simples")
 
-if opcao == 1:
-   num5=int(input("Digite o primeiro numero"))
-   num6=int(input("Digite o segundo numero"))
+opcao=input("voce deseja 1-soma 2-subtração 3-multiplicação 4-divisão ")
+
+if opcao == "1":
+   num5=int(input("Digite o primeiro numero "))
+   num6=int(input("Digite o segundo numero "))
    som= num5+num6
-   print(f"resultado da soma de numero{num5}+ {num6}={som}")
-elif opcao==2:
-   num5=int(input("Digite o primeiro numero"))
-   num6=int(input("Digite o segundo numero"))
+   print(f"resultado da soma de numero {num5} + {num6} = {som}")
+
+elif opcao== "2":
+   num5=int(input("Digite o primeiro numero "))
+   num6=int(input("Digite o segundo numero "))
    som= num5-num6
-   print(f"resultado da soma de numero{num5}-{num6}={som}")
-elif opcao==3:
-   num5=int(input("Digite o primeiro numero"))
-   num6=int(input("Digite o segundo numero"))
+   print(f"resultado da soma de numero {num5} - {num6} = {som}")
+elif opcao== "3":
+   num5=int(input("Digite o primeiro numero "))
+   num6=int(input("Digite o segundo numero "))
    som= num5*num6
-   print(f"resultado da soma de numero{num5}* {num6}={som}")
-elif opcao==4:
-   num5=int(input("Digite o primeiro numero"))
-   num6=int(input("Digite o segundo numero"))
+   print(f"resultado da soma de numero {num5} * {num6} = {som}")
+elif opcao== "4":
+   num5=int(input("Digite o primeiro numero "))
+   num6=int(input("Digite o segundo numero "))
    som= num5/num6
-   print(f"resultado da soma de numero{num5}/ {num6}={som}")
-  
+   print(f"resultado da soma de numero {num5} / {num6} = {som}")
+
+
 print("##############################################################")
 #Número Primo
 print("Número Primo") 
